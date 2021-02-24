80624f27db1aed03641c46fd4c5e98f7e6675c4c
David Gröger <david.groeger@xain.io>
2021-02-24 16:24:36 +0100

    top

--- commands/top.js
+++ commands/top.js
@@ -1,12 +1,13 @@
 const UserRepository = require('../repositories/userRepository');
 const LevelService = require('../services/levelService');
+const User = require('../models/user');
 
 module.exports = {
     name: 'top',
     description: 'Display the current top 10 level ranks.',
     async execute(client, message, args, Discord) {
        
-        let topUsers = await Review.find()
+        let topUsers = await User.find()
         .sort({xp: 1})
         .limit(10)
         .then(topUsers => {
