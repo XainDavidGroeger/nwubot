c813e978d8c5ec54bba516f28a726175afeb16cc
David Gröger <david.groeger@xain.io>
2021-02-24 16:52:57 +0100

    finish level

--- commands/commands.js
+++ commands/commands.js
@@ -12,6 +12,7 @@ module.exports = {
             { name: '!rules', value: `Displays the server rules` },
             { name: '!levelinfo', value: `Display info about all the levels` },
             { name: '!level', value: `Display your current level status` },
+            { name: '!top', value: `Displays the top 10 players from the level system.` },
             { name: '!invite', value: `Creates a unique Invitelink for you. If someone follows your link to the server, you will receive some decent XP!` },
             { name: '!question YOUR_QUESTION', value: `Will bring up a question in the training-hall.` },
             { name: '!answer QUESTION_ID YOUR_ANSWER', value: `Answer an excisting question and if it helps the creator of the question you receive some nice XP!` },
