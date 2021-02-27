e1a16f9e40583641eb737878be686d24b8b96467
David Gröger <david.groeger@xain.io>
2021-02-27 14:49:41 +0100

    added xpinfo command

--- commands/xpinfo.js
+++ commands/xpinfo.js
@@ -7,9 +7,10 @@ module.exports = {
         let embed = new Discord.MessageEmbed()
         .setColor('#80FFFF')
         .setImage(config.images.levelinfo)
-        .setFooter(`Inviting a playing using a link from the bot. "!invite" should give you this link to give out. They will need to be new to discord and need to accept the rules for exp to be given out.\n`
-        + `Typing in #📣nwu-public-chat specifically (limited, do not spam)\n`
-        + `Answering questions in #questions (look in channel for examples or ask @eGloryDave for more specifics if needed.). Answers have to be approved by the person who asked the question to get exp.\n`
+        .addFields(
+            { name: 'Inviting', value: `Inviting a playing using a link from the bot. "!invite" should give you this link to give out. They will need to be new to discord and need to accept the rules for exp to be given out.` },
+            { name: 'Chatting', value: `Typing in #📣nwu-public-chat specifically (limited, do not spam)` },
+            { name: 'Answering questions', value: `Answering questions in #questions (look in channel for examples or ask @eGloryDave for more specifics if needed.). Answers have to be approved by the person who asked the question to get exp.` },
         );
         message.channel.send(embed);
     }
