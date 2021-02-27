d33eaa9f3e49ae4cdad524e9c25cd0302c59e3f1
David Gröger <david.groeger@xain.io>
2021-02-27 13:43:50 +0100

    updated messageReactionAdd

--- events/client/messageReactionAdd.js
+++ events/client/messageReactionAdd.js
@@ -70,49 +70,45 @@ async function checkQuestionAnswerLogic(Discord, client, reaction, user) {
         }
 
         if (question !== null) {
-
-
             if (questionUser.dailyQuestions < client.config.xp.questionBoostDailyLimit) {
                 if (reaction.message.partial) await reaction.message.fetch();
                 if (reaction.partial) await reaction.fetch();
                 if (!reaction.message.guild) return;
 
                 if (question.answered === false) {
-                    if (reaction.emoji.name === goodAnswerEmoji) {
 
-                        question.answered = true;
-                        question.answeredBy = reaction.message.author.id;
-                        question.answerId = reaction.message.id;
+                    question.answered = true;
+                    question.answeredBy = reaction.message.author.id;
+                    question.answerId = reaction.message.id;
 
-                        await question.save()
-                            .then(result => console.log())
-                            .catch(err => console.log());
+                    await question.save()
+                        .then(result => console.log())
+                        .catch(err => console.log());
 
-                        answer.markedByQuestion = true;
-                        await answer.save()
-                            .then(result => console.log())
-                            .catch(err => console.log());
+                    answer.markedByQuestion = true;
+                    await answer.save()
+                        .then(result => console.log())
+                        .catch(err => console.log());
 
 
-                        await xpService.gainXp(client.config.xp.questionAnswered, answerUser.id, reaction.message.channel, client);
-                        let successEmbed = new Discord.MessageEmbed()
-                            .setColor('#80FFFF')
-                            .setTitle(`Gratulation ${answerUser.username} your answer was marked by the creator and you gained ${client.config.xp.questionAnswered} XP!`)
-                            .setImage(client.config.images.acceptedMentor)
-                            .addFields(
-                                { name: 'Question', value: question.question },
-                                { name: 'Answer', value: answer.answer },
-                                { name: 'Bonus XP', value: client.config.xp.questionAnswered },
-                            );
+                    await xpService.gainXp(client.config.xp.questionAnswered, answerUser.id, reaction.message.channel, client);
+                    let successEmbed = new Discord.MessageEmbed()
+                        .setColor('#80FFFF')
+                        .setTitle(`Gratulation ${answerUser.username} your answer was marked by the creator and you gained ${client.config.xp.questionAnswered} XP!`)
+                        .setImage(client.config.images.acceptedMentor)
+                        .addFields(
+                            { name: 'Question', value: question.question },
+                            { name: 'Answer', value: answer.answer },
+                            { name: 'Bonus XP', value: client.config.xp.questionAnswered },
+                        );
 
-                        questionUser.dailyQuestions++;
-                        await questionUser.save()
-                            .then(result => console.log())
-                            .catch(err => console.log());
+                    questionUser.dailyQuestions++;
+                    await questionUser.save()
+                        .then(result => console.log())
+                        .catch(err => console.log());
 
-                        sendToMessageChannel.send(successEmbed);
-                        answerUser.send(successEmbed);
-                    }
+                    sendToMessageChannel.send(successEmbed);
+                    answerUser.send(successEmbed);
                 }
             } else {
                 if (reaction.message.partial) await reaction.message.fetch();
