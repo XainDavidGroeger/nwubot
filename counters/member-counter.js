0e3efb2eaffa1064267184a8904b16389a37cf94
David Gröger <david.groeger@xain.io>
2021-03-02 22:26:04 +0100

    counter fix

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -3,7 +3,7 @@ module.exports = async (client) => {
     const guild = client.guilds.cache.get(process.env.GUILD_ID);
 
     const goalChannel = guild.channels.cache.get(process.env.MEMBER_COUNT_GOAL_CHANNEL_ID);
-    goalChannel.setName(`Goal: 200`);
+    goalChannel.setName(`Goal: 250`);
 
     const serverBoostChannel = guild.channels.cache.get(process.env.SERVER_BOOST_LEVEL_CHANNEL_ID);
     serverBoostChannel.setName(`Server Boost Level: ${guild.premiumTier}`);
