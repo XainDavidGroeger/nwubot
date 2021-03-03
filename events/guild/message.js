d315af3b8b644120eba62d8601bf9c60deeb330a
David Gröger <david.groeger@xain.io>
2021-03-03 15:37:07 +0100

    added swear words

--- events/guild/message.js
+++ events/guild/message.js
@@ -14,26 +14,26 @@ module.exports = async (Discord, client, message) => {
     messageContentArray = messageContent.split(' ');
 
 
-  
+
     let newMessage = "";
     let includesBadWords = false;
-    messageContentArray.forEach(function(oneData) {
+    messageContentArray.forEach(function (oneData) {
         let commata = " ";
         if (newMessage != "") {
             commata = " , ";
         }
         if (client.swearwords.includes(oneData)) {
-            includesBadWords=true;
-            newMessage = newMessage +  commata +  "****";
+            includesBadWords = true;
+            newMessage = newMessage + commata + "****";
         } else {
             newMessage = newMessage + commata + oneData;
         }
     });
-    
 
-   console.log(message.content)
-   console.log(includesBadWords)
-   console.log(newMessage)
+
+    console.log(message.content)
+    console.log(includesBadWords)
+    console.log(newMessage)
 
     if (!message.content.startsWith(prefix)
         && message.channel.id === process.env.GENERAL_CHANNEL
@@ -42,22 +42,24 @@ module.exports = async (Discord, client, message) => {
         await xpService.gainXp(config.xp.message, message.author.id, message.channel, client);
     }
 
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const cmd = args.shift().toLowerCase();
-
-    const command = client.commands.get(cmd);
-
-
-    if (command) command.execute(client, message, args, Discord);
-
 
     // delete swear message and send new message with **** instead of swear words
-    if(includesBadWords) {
+    if (includesBadWords) {
         message.channel.send(newMessage)
+        message.channel.send(`<@${message.author.id}> please mind your words, i had to replace some of your dirty expressions.`);
         message.delete();
+    } else {
+        if (!message.content.startsWith(prefix)) return;
+
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const cmd = args.shift().toLowerCase();
+
+        const command = client.commands.get(cmd);
+
+
+        if (command) command.execute(client, message, args, Discord);
     }
+
 
 }
 
