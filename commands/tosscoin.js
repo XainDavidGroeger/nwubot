e84cb4a8d137ca1979a17fa76881d069c21b476d
David Gröger <david.groeger@xain.io>
2021-03-03 16:16:21 +0100

    added swear words

---
+++ commands/tosscoin.js
@@ -0,0 +1,28 @@
+const config = require('../config.json');
+
+module.exports = {
+    name: 'tosscoin',
+    description: 'Toss a coin, randomly picks head or tail',
+    execute(client, message, args, Discord) {
+       
+        let tossedCoin =  Math.floor(Math.random() * 1); 
+        console.log(tossedCoin);
+        let image = "";
+        let tossedName = "";
+        if (tossedCoin == 0) {
+            image = cleint.config.images.tossedhead;
+            tossedName = "head";
+        } else {
+            image = cleint.config.images.tossedtail;
+            tossedName = "tail";
+        }
+
+        let embed = new Discord.MessageEmbed()
+            .setColor('#80FFFF')
+            .setTitle(`<@${message.author.username}> tossed ${tossedName}`)
+            .setImage(image)
+        ;
+        message.channel.send(embed);
+        
+    }
+}
