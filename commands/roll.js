a4fb3150b62c8c5a748c906d980f21309e6310d3
David Gröger <david.groeger@xain.io>
2021-03-04 17:17:55 +0100

    roll

--- commands/roll.js
+++ commands/roll.js
@@ -3,21 +3,34 @@ const config = require('../config.json');
 module.exports = {
     name: 'roll',
     description: 'daily random shinobi roll',
-    execute(client, message, args, Discord) {
+    async execute(client, message, args, Discord) {
+
+        let user = await UserRepository.createOrFindUser(message.author.id, message.channel, client);
+
+        if (user.dailyRollUsed == 0) {
+            
+            let randomId =  Math.floor(Math.random() * 76); 
+            let shinobi = client.config.heroes[randomId];
+    
+            console.log(randomId)
+            console.log(shinobi)
+    
+            let embed = new Discord.MessageEmbed()
+                .setColor('#80FFFF')
+                .setTitle(`${message.author.username} you rolled ${shinobi.name}, you gained ${shinobi.xp} XP!`)
+                .setImage(shinobi.image)
+            ;
+            message.channel.send(embed);
+
+            user.dailyRollUsed = 1;
+            await user.save()
+            .then(result => console.log())
+            .catch(err => console.log(err));
+    
+        } else {
+            message.channel.send(`You already rolled today, try your luck again tomorrow!`);
+        }
        
-        let randomId =  Math.floor(Math.random() * 76); 
-        let shinobi = client.config.heroes[randomId];
-
-        console.log(randomId)
-        console.log(shinobi)
-
-        let embed = new Discord.MessageEmbed()
-            .setColor('#80FFFF')
-            .setTitle(`${message.author.username} you rolled ${shinobi.name}, you gained ${shinobi.xp} XP!`)
-            .setImage(shinobi.image)
-        ;
-        message.channel.send(embed);
-
         return true;
     }
 
