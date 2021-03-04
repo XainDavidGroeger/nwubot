0a137f349397b70dac67bb86ffd95ffcfb1ab02a
David Gröger <david.groeger@xain.io>
2021-03-04 17:05:41 +0100

    roll

--- commands/roll.js
+++ commands/roll.js
@@ -6,13 +6,20 @@ module.exports = {
     execute(client, message, args, Discord) {
        
 
-
         let randomId =  Math.floor(Math.random() * 76); 
 
 
         let shinobi = client.config.heroes[randomId];
 
+        console.log(randomId)
         console.log(shinobi)
+
+        let embed = new Discord.MessageEmbed()
+            .setColor('#80FFFF')
+            .setTitle(`${message.author.username} rolled ${shinobi.name}, you gain ${shinobi.xp}!`)
+            .setImage(shinobi.image)
+        ;
+        message.channel.send(embed);
 
         return true;
 
