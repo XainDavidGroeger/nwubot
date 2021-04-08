52b8eed0296698fabb1bdec291e92e1c6b75f324
LearningDave <38720791+XainDavidGroeger@users.noreply.github.com>
2021-04-09 01:50:16 +0200

    added new shedules

--- commands/mute.js
+++ commands/mute.js
@@ -13,12 +13,6 @@ module.exports = {
         if (message.member.roles.cache.find(role => role.name === client.config.roles.moderator)) {
 
             
-        await User.updateMany({
-            dailyRollUsed: 0,
-            __v: 0,
-        });
-        client.guilds.cache.get(process.env.GUILD_ID).channels.cache.get(process.env.GENERAL_CHANNEL).send('Time to roll again, give it a shot with !roll.')
-
             const target = message.mentions.users.first();
 
             if (target) {
